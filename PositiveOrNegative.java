eeb593b3d001422a0a8e72d302d66b44cf151830
Kalaivani <kalaivani.kcse@ksrce.ac.in>
2016-07-19 17:00:40 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,11 @@
+class PositiveOrNegative{
+public void main(String[] args){
+int n=10;
+if(n<0)
+  System.out.prinltn("Negative");
+  else if(n>0)
+   System.out.println("Positive");
+   else
+   System.out.println("Number is Zero");
+   }
+}
